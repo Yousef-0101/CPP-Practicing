de30c5c4fd3687e70ce928cabd377a87c23990f7
Yousef <20912024100637@fci.zu.edu.eg>
2026-04-13 23:25:27 +0200

    Add initial implementation of app.cpp and T1 and T2.cpp with basic output

---
+++ Drafts/Intro (01 To 08)/app.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int x = 4;
+    printf("World Hello!\n");
+    std::cout << "Hello \n";
+    std::cout << "Hello ";
+    std::cout << "Hello \n";
+    return 0;
+}
