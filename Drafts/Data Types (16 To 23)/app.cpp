ef8558c61a789212566ef213211ae778979536da
Yousef <20912024100637@fci.zu.edu.eg>
2026-04-28 03:58:14 +0300

    notes on type alias and type modifires

---
+++ Drafts/Data Types (16 To 23)/app.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    using bignumber = long long; // Type Alias
+    typedef long long bignum;    // Type Alias
+
+    int num_one = 100000000000000000; // problem (integer overflows)
+    long long num_two = 100000000000000000;
+    bignumber num_three = 100000000000000000;
+    bignum num_four = 100000000000000000;
+
+    cout << num_one << endl;
+    cout << num_two << endl;
+    cout << num_three << endl;
+    cout << num_four << endl;
+
+    cout << sizeof(short) << endl;
+    cout << sizeof(long) << endl;
+    cout << sizeof(long long) << endl;
+    return 0;
+}
